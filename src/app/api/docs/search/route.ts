c77dcd72aa569f364ffd0ea2bdc390ef8424b99b
menghanjun <1305850439@qq.com>
2025-09-16 20:10:34 +0800

    Update project configuration and API routes

--- src/app/api/docs/search/route.ts
+++ src/app/api/docs/search/route.ts
@@ -3,5 +3,6 @@ import { createFromSource } from "fumadocs-core/search/server";
 
 export const { GET } = createFromSource(source, {
   // https://docs.orama.com/open-source/supported-languages
+  // Use english for all languages since Orama doesn't support Chinese
   language: "english",
 });
